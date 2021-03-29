f98b6463c3a9ab3c901156b840429169fd654eb4
julcaso <julioenriquecastro@gmail.com>
2021-03-29 10:25:47 -0400

    Add files via upload

---
+++ Serie0.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Serie0{
+    public static void main (String args []){
+
+        double x=0.0;
+        double r = 0.0;
+        double factorial=1.0;
+
+        //entrada de datos.
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Entre valor de x: ");
+        x = scn.nextDouble();
+
+        //Calculo de la expresion matematica.
+        for(int i=0; i<=8; i++){
+            //calcular factorial. 
+            for (int j = 0; j<=i; j++){
+                if (j == 0) {
+                    factorial = 1.0;
+                } else {
+                    factorial = factorial * j;
+                }
+            }
+            //Realiza acumulacion de expresion matematica            
+            r = r + Math.pow(x,i)/factorial;
+            System.out.println("Resultado de expresion matematica: " + r);
+        }
+
+        
+    }
+}
