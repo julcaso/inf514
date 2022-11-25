0f43a873b3e9726faf583d5c0c892f874082e89a
julcaso <julioenriquecastro@gmail.com>
2022-11-24 20:34:48 -0400

    Create StarNBAPlayers.java

---
+++ StarNBAPlayers.java
@@ -0,0 +1,18 @@
+public class StarNBAPlayers{
+    public static void main(String [] args){
+        if (args.length !=0){
+            if (args[0].equalsIgnoreCase("-H")) {
+                System.out.println("--------------------------------");
+                System.out.println("Ayuda del comando StarNBAPlayers");
+                System.out.println("--------------------------------\n");
+                System.out.println("DESCRIPCION \n");
+                System.out.println("Este progarma despliega las estrellas de los equipos de la NBA.\n");
+                System.out.println("EJEMPLO \n");
+                System.out.println("java StarNBAPlayers GoldenState\n");
+                System.out.println("La salida seria: \n");
+                System.out.println("Stephen Curry\n");
+            } else if (args[0])
+        }
+    }
+
+}
