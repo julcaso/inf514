9774c521cd7d1866651b2bd23d66cb64b5f66486
julcaso <julioenriquecastro@gmail.com>
2022-11-24 21:21:15 -0400

    Update StarNBAPlayers.java

--- StarNBAPlayers.java
+++ StarNBAPlayers.java
@@ -1,7 +1,6 @@
 public class StarNBAPlayers{
-    public static void main(String [] args){
-        if (args.length !=0){
-            if (args[0].equalsIgnoreCase("-H")) {
+    public static void main(String [] args){        
+            if (args.length == 0 || args[0].equalsIgnoreCase("-H")) {
                 System.out.println("--------------------------------");
                 System.out.println("Ayuda del comando StarNBAPlayers");
                 System.out.println("--------------------------------\n");
@@ -11,8 +10,10 @@ public class StarNBAPlayers{
                 System.out.println("java StarNBAPlayers GoldenState\n");
                 System.out.println("La salida seria: \n");
                 System.out.println("Stephen Curry\n");
-            } else if (args[0])
-        }
+            } else if (args[0].equalsIgnoreCase("GoldenState")){
+                System.out.println("El jugador estrella es: Stephen Curry.");
+            } else if (args[0].equalsIgnoreCase("Boston")){
+                System.out.println("El jugador estrella es: Al Hordford.");
+            }
     }
-
 }
